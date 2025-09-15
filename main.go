f5daad66379041a3c9da2934ceaad23e42757767
EgorLavrega <sharingansou@mail.ru>
2025-09-15 17:09:01 +0300

    home 1

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import "fmt"
+
 func main() {
-	return
+	const usdToEur = 0.85
+	const usdToRub = 84.1
+	const eurToRub = usdToRub / usdToEur
+	fmt.Printf("1 Usd = %.2f Eur\n", usdToEur)
+	fmt.Printf("1 Usd = %.2f Rub\n", usdToRub)
+	fmt.Printf("1 Eur = %.2f Rub\n", eurToRub)
 }
